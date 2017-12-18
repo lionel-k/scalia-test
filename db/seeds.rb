51494f4a72a03c708c188e09c964ff158fb9a1a3
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-19 00:17:15 +0100

    added 2nd user to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,13 @@ Price.create(currency: 'Eur', value: '40', product: nike)
 Price.create(currency: 'Dollars', value: '49', product: nike)
 Price.create(currency: 'Eur', value: '30', product: addidas)
 Price.create(currency: 'Dollars', value: '37', product: addidas)
+
+
+mark = User.create(username: 'Mark')
+hp = Product.create(name: 'HP Elite 820P', size: '13', user: mark)
+asus = Product.create(name: 'Asus E403NA-FA049T', size: '17', user: mark)
+
+Price.create(currency: 'Eur', value: '400', product: hp)
+Price.create(currency: 'Dollars', value: '490', product: hp)
+Price.create(currency: 'Eur', value: '300', product: asus)
+Price.create(currency: 'Dollars', value: '370', product: asus)
