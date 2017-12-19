95845527b92030bf1e28ca79f328f15e87eaa018
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-19 17:14:31 +0100

    add test of creating a new product

--- part-2/test/system/products_test.rb
+++ part-2/test/system/products_test.rb
@@ -6,4 +6,14 @@ class ProductsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "All Products"
     assert_selector ".product", count: Product.count
   end
+
+
+  test "lets a signed in user create a new product" do
+    login_as users(:toto)
+    visit "/products/new"
+    click_on 'Create!'
+
+    # Should be redirected to show page of the last product created
+    assert_equal product_path(Product.last), page.current_path
+  end
 end
