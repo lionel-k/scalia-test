8800859d612c8346e83c01565508c1d97eee37df
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-19 17:07:55 +0100

    added 2 tests - h1 et count products on index page

--- part-2/test/system/products_test.rb
+++ part-2/test/system/products_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit products_url
-
     assert_selector "h1", text: "All Products"
+    assert_selector ".product", count: Product.count
   end
 end
